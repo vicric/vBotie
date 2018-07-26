aaed3d8e01d5972f877bc1a6639f20c0c65dd279
Vicent Rico Rico <vice.rico@gmail.com>
2018-07-26 17:40:33 +0200

    Meme bot

--- vBotie-src/Dialogs/EchoDialog.cs
+++ vBotie-src/Dialogs/EchoDialog.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using Microsoft.Bot.Connector;
 using Microsoft.Bot.Builder.Dialogs;
 using System.Net.Http;
-
+using SimpleEchoBot;
 
 namespace Microsoft.Bot.Sample.SimpleEchoBot
 {
@@ -13,6 +13,11 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
     public class EchoDialog : IDialog<object>
     {
         protected int count = 1;
+
+        private MemeAPI _api = new MemeAPI();
+        private Meme _meme;
+        private string _topText;
+        private string _bottomText;
 
         public async Task StartAsync(IDialogContext context)
         {
@@ -23,7 +28,7 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
         {
             var message = await argument;
 
-            if (message.Text == "reset")
+            if (message.Text.ToLower() == "reset")
             {
                 PromptDialog.Confirm(
                     context,
@@ -32,14 +37,14 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
                     "Didn't get that!",
                     promptStyle: PromptStyle.Auto);
             }
-            else if (message.Text == "set")
+            else if (message.Text.ToLower() == "set")
             {
                 PromptDialog.Number(
                     context,
                     AfterSetAsync,
                     "What number do you want to set the count to?");
             }
-            else if (message.Text == "set choice")
+            else if (message.Text.ToLower() == "set choice")
             {
                 var options = new int[] {10, 100, 1000, 3000};
                 var descriptions = new string[]{"Set count to 10", "Set count to 100", "Set count to 1000", "Set count to 3000"};
@@ -50,12 +55,19 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
                     "What number do you want to set the count to?",
                     descriptions: descriptions );
             }
-            else if (message.Text == "attachment")
+            else if (message.Text.ToLower() == "attachment")
             {
                 PromptDialog.Attachment(
                     context,
                     AfterAttachmentAsync,
                     "Okey, send me an attachment");
+            }
+            else if (message.Text.ToLower() == "memes")
+            {
+                var memes = new Meme[] { Meme.Brian, Meme.Fry, Meme.GrumpyCat, Meme.OMG, Meme.Spiderman, Meme.Troll, Meme.Wonka };
+                var descriptions = new string[] { "Bad luck Brian", "Fry", "Grumpy Cat", "OMG", "Spiderman", "Troll", "Wonka" };
+                PromptDialog.Choice<Meme>(context, ResumeAfterMemeSelectionClarification,
+                    memes, "Hi, I'm MemeBot. Which meme should I generate?", descriptions: descriptions);
             }
             else
             {
@@ -63,6 +75,8 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
                 context.Wait(MessageReceivedAsync);
             }
         }
+
+        #region Counter
 
         public async Task AfterResetAsync(IDialogContext context, IAwaitable<bool> argument)
         {
@@ -107,6 +121,42 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
             
             context.Wait(MessageReceivedAsync);
         }
+
+        #endregion
+
+        #region MEME
+
+        private async Task ResumeAfterMemeSelectionClarification(IDialogContext context, IAwaitable<Meme> result)
+        {
+            _meme = await result;
+            PromptDialog.Text(context, ResumeAfterTopTextClarification, "Awesome, I can work with that. What will the *top text* be?");
+        }
+
+        private async Task ResumeAfterTopTextClarification(IDialogContext context, IAwaitable<string> result)
+        {
+            _topText = await result;
+            PromptDialog.Text(context, ResumeAfterBottomTextClarification, "Good, now what will the *bottom text* be?");
+        }
+
+        private async Task ResumeAfterBottomTextClarification(IDialogContext context, IAwaitable<string> result)
+        {
+            _bottomText = await result;
+
+            // setup a message that can hold the generated image
+            var replyMessage = context.MakeMessage();
+            var image = new Attachment();
+            image.ContentType = "image/jpeg";
+            image.ContentUrl = $"data:image/jpeg;base64,{ await _api.GenerateMeme(_meme, _topText, _bottomText) }";
+            replyMessage.Attachments = new List<Attachment> { image };
+
+            // return our reply to the user
+            await context.PostAsync(replyMessage);
+            await context.PostAsync("I hope you like your meme. If not, generate a new one!");
+
+            context.Wait(MessageReceivedAsync);
+        }
+
+        #endregion
 
     }
 }
---
+++ vBotie-src/MemeAPI.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using unirest_net.http;
+using System.IO;
+
+namespace SimpleEchoBot
+{
+    public enum Meme
+    {
+        Brian, Fry, GrumpyCat, OMG, Spiderman, Troll, Wonka
+    }
+
+    [Serializable]
+    public class MemeAPI
+    {
+        private static string RAPID_API_KEY = "8NmTGel7FkmshxbtzpazdsElFbR2p1aItGMjsnPFpBB9sEWSLo";
+
+        /// <summary>
+        /// This methods calls the APIMeme ( http://apimeme.com/ ) through RapidAPI ( https://rapidapi.com/user/ronreiter/package/Meme%20Generator )
+        /// and returns the generated meme as Base64String
+        /// </summary>
+        public async Task<string> GenerateMeme(Meme meme, string topText, string bottomText)
+        {
+            var response = await Unirest.get($"https://ronreiter-meme-generator.p.mashape.com/meme?bottom={ HttpUtility.HtmlEncode(bottomText) }&font=Impact&font_size=50&meme={ ConvertMemeToURL(meme) }&top={ HttpUtility.HtmlEncode(topText) }")
+                .header("X-Mashape-Key", RAPID_API_KEY)
+                .header("X-Mashape-Host", "ronreiter-meme-generator.p.mashape.com")
+            .asJsonAsync<MemoryStream>();
+
+            return Convert.ToBase64String(response.Body.ToArray());
+        }
+
+        private string ConvertMemeToURL(Meme meme)
+        {
+            switch (meme)
+            {
+                case Meme.Brian:
+                    return "Bad-Luck-Brian";
+                case Meme.Fry:
+                    return "Futurama-Fry";
+                case Meme.GrumpyCat:
+                    return "Grumpy-Cat";
+                case Meme.OMG:
+                    return "OMG-Cat";
+                case Meme.Spiderman:
+                    return "Spiderman-Computer-Desk";
+                case Meme.Troll:
+                    return "Troll-Face";
+                case Meme.Wonka:
+                    return "Condescending-Wonka";
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
